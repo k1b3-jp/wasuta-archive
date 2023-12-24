f57e91cc10b279bf0c710f95fc86ca2388748ec2
k1b3-jp <exfamily0315@gmail.com>
2023-12-24 11:59:52 +0900

    Add <SpeedInsights/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
