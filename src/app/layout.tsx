fc7fbc134e8ad222eddef452ea8ef6dcd3ad9d4e
k1b3-jp <exfamily0315@gmail.com>
2023-12-24 16:54:50 +0900

    coordinate front directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import supabase from '../utils/supabaseClient'
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.js または pages/index.tsx
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to Next.js!</h1>
+        <p>This is the home page.</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
