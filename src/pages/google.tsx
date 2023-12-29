061e8ed10a5faeddbbfe627a5b9b284dec456209
k1b3-jp <exfamily0315@gmail.com>
2023-12-29 17:24:50 +0900

    fix auth theme

--- src/pages/google.tsx
+++ src/pages/google.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-// import styles from "../styles/Home.module.css";
 import { createClient } from '@supabase/supabase-js'
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { Auth } from '@supabase/auth-ui-react'
+import {
+    ThemeSupa,
+  } from '@supabase/auth-ui-shared'
 import supabase from '../utils/supabaseClient'
 
 export default function Google(){
-//   const supabase = createClient(SupabaseClient.supabaseUrl, 'Project API anon key')
-
   return (
     <>
           <Auth
