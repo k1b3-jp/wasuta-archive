47a766bb9aa2f3e80c830ed0e42d27bcaba38d84
k1b3-jp <exfamily0315@gmail.com>
2023-12-30 16:29:34 +0900

    add Tag component

---
+++ src/components/ui/Tag.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface TagProps {
+  label: string;
+  selected: boolean;
+  onSelect: () => void;
+}
+
+const Tag: React.FC<TagProps> = ({ label, selected, onSelect }) => {
+  const selectedStyles = 'bg-blue-600 text-white';
+  const unselectedStyles = 'bg-blue-100 text-blue-600';
+
+  const buttonStyles = selected ? selectedStyles : unselectedStyles;
+
+  return (
+    <button
+      onClick={onSelect}
+      className={`text-sm font-medium px-4 py-2 rounded-full transition duration-300 ease-in-out ${buttonStyles}`}
+      aria-pressed={selected}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Tag;
