9ea8b6d53926ff51364128907a61debd01901f14
k1b3-jp <exfamily0315@gmail.com>
2023-12-30 16:06:43 +0900

    add BaseButton

---
+++ src/components/ui/BaseButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface BaseButtonProps {
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const BaseButton: React.FC<BaseButtonProps> = ({ label, onClick, disabled }) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out ${
+        disabled ? 'opacity-50 cursor-not-allowed' : ''
+      }`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default BaseButton;
+
