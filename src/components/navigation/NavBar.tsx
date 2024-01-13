7a1b99753556e88961c95219d54dde8153c503bd
k1b3-jp <exfamily0315@gmail.com>
2024-01-13 16:24:30 +0900

    fix NavBar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -63,7 +63,7 @@ const NavBar = () => {
           } top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto`}
           id="navigation"
         >
-          <div class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
             <Link
               href="/events"
               className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
