d24009f2f5e59df9d7c866f064750805860caf08
k1b3-jp <exfamily0315@gmail.com>
2024-02-10 13:41:58 +0900

    fix seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -28,18 +28,8 @@ values
 ('奈々聖'),
 ('瑠香'),
 ('美里'),
-('梨々華');
-
--- Event_Tagsテーブルにサンプルデータを追加
-insert into public.Event_Tags (event_id, tag_id)
-values 
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5),
-(6, 6),
-(7, 7);
+('梨々華'),
+('葉月');
 
 -- Youtube_Tag_Namesテーブルにサンプルデータを追加
 insert into public.Youtube_Tag_Names (name)
@@ -48,11 +38,3 @@ values
 ('最前'),
 ('全編'),
 ('1曲');
-
--- Youtube_Tagsテーブルにサンプルデータを追加
-insert into public.Youtube_Tags (youtube_link_id, tag_id)
-values 
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
