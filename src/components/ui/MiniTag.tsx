be7e57742d2f3a3d5c47d76436acf8a24625f7e4
k1b3-jp <exfamily0315@gmail.com>
2024-02-11 18:25:14 +0900

    add switching tag color

--- src/components/ui/MiniTag.tsx
+++ src/components/ui/MiniTag.tsx
@@ -5,8 +5,30 @@ interface TagProps {
 }
 
 const Tag: React.FC<TagProps> = ({ label }) => {
+  let tagStyles;
+  switch (label) {
+    case '奈々聖':
+      tagStyles = 'bg-light-green text-white';
+      break;
+    case '瑠香':
+      tagStyles = 'bg-pink text-white';
+      break;
+    case '美里':
+      tagStyles = 'bg-purple text-white';
+      break;
+    case '梨々華':
+      tagStyles = 'bg-light-blue text-white';
+      break;
+    case '葉月':
+      tagStyles = 'bg-yellow text-white';
+      break;
+    default:
+      tagStyles = 'bg-gray-100 text-gray-800';
+      break;
+  }
+
   return (
-    <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-3 py-1 rounded-2xl ms-1">
+    <span className={`text-sm font-medium me-2 px-3 py-1 rounded-2xl ms-1 ${tagStyles}`}>
       {label}
     </span>
   );
