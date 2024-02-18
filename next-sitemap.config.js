ebfa54fcdcab2df17c91a257799aaed69c942c95
k1b3-jp <exfamily0315@gmail.com>
2024-02-18 12:22:22 +0900

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.wasuta-archive.com/',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './src/app',
+};
