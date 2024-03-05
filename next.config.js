d358645f49b6447a4e5784216b3fc1bb350204b4
k1b3-jp <exfamily0315@gmail.com>
2024-03-05 19:44:23 +0900

    add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,24 +1,35 @@
-const { default: next } = require('next');
+const { default: next } = require("next");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    // BundleAnalyzerPluginの条件付き使用
+    if (process.env.ANALYZE === "true") {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "server",
+          analyzerPort: isServer ? 8888 : 8889,
+          openAnalyzer: true,
+        })
+      );
+    }
+
+    // SVGファイルのためのルールを追加
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-        },
-      ],
+      use: ["@svgr/webpack"],
     });
+
     return config;
   },
   images: {
-    domains: ['127.0.0.1'],
-    disableStaticImages: true, // importした画像の型定義設定を無効にする
+    domains: ["127.0.0.1"],
+    disableStaticImages: true,
   },
   env: {
-    defaultOgpImage: 'https://www.wasuta-archive.com/opengraph-image.png',
+    defaultOgpImage: "https://www.wasuta-archive.com/opengraph-image.png",
   },
 };
 
