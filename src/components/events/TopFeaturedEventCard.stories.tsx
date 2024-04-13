6acca80a8e28488e35ac3dbf7b25953ad2f7f879
k1b3-jp <exfamily0315@gmail.com>
2024-04-13 14:57:29 +0900

    lint

--- src/components/events/TopFeaturedEventCard.stories.tsx
+++ src/components/events/TopFeaturedEventCard.stories.tsx
@@ -4,21 +4,21 @@ import TopFeaturedEventCard from "./TopFeaturedEventCard";
 import type EventCard from "./EventCard";
 
 export default {
-  title: "Components/TopFeaturedEventCard",
-  component: TopFeaturedEventCard,
-  parameters: {
-    layout: "centered",
-  },
+	title: "Components/TopFeaturedEventCard",
+	component: TopFeaturedEventCard,
+	parameters: {
+		layout: "centered",
+	},
 } as Meta;
 
 const Template: Story<typeof EventCard> = (args) => (
-  <TopFeaturedEventCard {...args} />
+	<TopFeaturedEventCard {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  title: "イベントタイトル",
-  location: "イベント開催場所",
-  date: "JUL 04",
-  imageUrl: "https://wa-suta.world/assets/img/top/img_mainvisual.jpg?20231127",
+	title: "イベントタイトル",
+	location: "イベント開催場所",
+	date: "JUL 04",
+	imageUrl: "https://wa-suta.world/assets/img/top/img_mainvisual.jpg?20231127",
 };
