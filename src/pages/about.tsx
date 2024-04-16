5b7781e34890c58cf0ee69633894fc2387cb6386
k1b3-jp <exfamily0315@gmail.com>
2024-04-16 19:22:36 +0900

    fix color

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -22,7 +22,7 @@ export default function About() {
         <section className="py-12 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="lg:text-center">
-              <h2 className="font-heading mb-4 bg-light-green text-deep-green px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font">
+              <h2 className="font-heading mb-4 bg-deep-green text-white px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest uppercase title-font">
                 わーすたアーカイブとは？
               </h2>
               <p className="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
