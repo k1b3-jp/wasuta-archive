31dc5a63c6c65ebf513415186c55d4ff44f72a64
k1b3-jp <exfamily0315@gmail.com>
2024-05-11 14:02:34 +0900

    biome format

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -7,41 +7,41 @@ import Link from "next/link";
 const defaultImageUrl = "/event-placeholder.png";
 
 const EventCard: React.FC<EventCardProps> = ({
-  title,
-  location,
-  date,
-  imageUrl,
-  id,
+	title,
+	location,
+	date,
+	imageUrl,
+	id,
 }) => {
-  return (
-    <div className="max-w-xs bg-white rounded-xl shadow-md overflow-hidden max-w-sm relative">
-      <Link href={`/events/${id}`} rel="noopener noreferrer" target="_blank">
-        <div className="overflow-hidden">
-          <img
-            src={imageUrl || defaultImageUrl}
-            alt={title}
-            width={320}
-            height={208}
-            className="hover:scale-110 transition-all duration-500 w-full h-52 object-cover"
-          />
-        </div>
-        <div className="absolute top-0 right-0 py-1 px-3 rounded-bl-lg bg-light-gray">
-          {formatDate(date)}
-        </div>
-        <div className="p-4">
-          <div className="mb-2">
-            <div className="text-md font-semibold line-clamp-2 h-12">
-              {title}
-            </div>
-          </div>
-          <div className="text-sm text-gray-500 mb-6 line-clamp-1 leading-7 h-7">
-            {location}
-          </div>
-          <BaseButton label="詳細を見る" />
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div className="max-w-xs bg-white rounded-xl shadow-md overflow-hidden max-w-sm relative">
+			<Link href={`/events/${id}`} rel="noopener noreferrer" target="_blank">
+				<div className="overflow-hidden">
+					<img
+						src={imageUrl || defaultImageUrl}
+						alt={title}
+						width={320}
+						height={208}
+						className="hover:scale-110 transition-all duration-500 w-full h-52 object-cover"
+					/>
+				</div>
+				<div className="absolute top-0 right-0 py-1 px-3 rounded-bl-lg bg-light-gray">
+					{formatDate(date)}
+				</div>
+				<div className="p-4">
+					<div className="mb-2">
+						<div className="text-md font-semibold line-clamp-2 h-12">
+							{title}
+						</div>
+					</div>
+					<div className="text-sm text-gray-500 mb-6 line-clamp-1 leading-7 h-7">
+						{location}
+					</div>
+					<BaseButton label="詳細を見る" />
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default EventCard;
--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,57 +1,57 @@
 import type React from "react";
 
 interface TagProps {
-  label: string;
-  selected: boolean;
-  onSelect: () => void;
+	label: string;
+	selected: boolean;
+	onSelect: () => void;
 }
 
 const Tag: React.FC<TagProps> = ({ label, selected, onSelect }) => {
-  const stylesMap: { [key: string]: { selected: string; unselected: string } } =
-    {
-      奈々聖: {
-        selected: "bg-light-green text-white",
-        unselected: "bg-white text-light-green border border-light-green",
-      },
-      瑠香: {
-        selected: "bg-pink text-white",
-        unselected: "bg-white text-pink border border-pink",
-      },
-      美里: {
-        selected: "bg-purple text-white",
-        unselected: "bg-white text-purple border border-purple",
-      },
-      梨々華: {
-        selected: "bg-light-blue text-white",
-        unselected: "bg-white text-light-blue border border-light-blue",
-      },
-      葉月: {
-        selected: "bg-yellow text-white",
-        unselected: "bg-white text-yellow border border-yellow",
-      },
-      default: {
-        selected: "bg-gray-500 text-white",
-        unselected: "bg-white text-gray-500 border border-gray-500",
-      },
-    };
+	const stylesMap: { [key: string]: { selected: string; unselected: string } } =
+		{
+			奈々聖: {
+				selected: "bg-light-green text-white",
+				unselected: "bg-white text-light-green border border-light-green",
+			},
+			瑠香: {
+				selected: "bg-pink text-white",
+				unselected: "bg-white text-pink border border-pink",
+			},
+			美里: {
+				selected: "bg-purple text-white",
+				unselected: "bg-white text-purple border border-purple",
+			},
+			梨々華: {
+				selected: "bg-light-blue text-white",
+				unselected: "bg-white text-light-blue border border-light-blue",
+			},
+			葉月: {
+				selected: "bg-yellow text-white",
+				unselected: "bg-white text-yellow border border-yellow",
+			},
+			default: {
+				selected: "bg-gray-500 text-white",
+				unselected: "bg-white text-gray-500 border border-gray-500",
+			},
+		};
 
-  const selectedStyles =
-    stylesMap[label]?.selected || stylesMap.default.selected;
-  const unselectedStyles =
-    stylesMap[label]?.unselected || stylesMap.default.unselected;
+	const selectedStyles =
+		stylesMap[label]?.selected || stylesMap.default.selected;
+	const unselectedStyles =
+		stylesMap[label]?.unselected || stylesMap.default.unselected;
 
-  const buttonStyles = selected ? selectedStyles : unselectedStyles;
+	const buttonStyles = selected ? selectedStyles : unselectedStyles;
 
-  return (
-    <button
-      type="button"
-      onClick={onSelect}
-      className={`text-sm font-medium px-4 py-2 rounded-full transition duration-300 ease-in-out ${buttonStyles}`}
-      aria-pressed={selected}
-    >
-      {label}
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			onClick={onSelect}
+			className={`text-sm font-medium px-4 py-2 rounded-full transition duration-300 ease-in-out ${buttonStyles}`}
+			aria-pressed={selected}
+		>
+			{label}
+		</button>
+	);
 };
 
 export default Tag;
