11ff247f4a0d16e68832f4f29308fe8ff72a435e
k1b3-jp <exfamily0315@gmail.com>
2024-05-22 20:07:34 +0900

    add dynamic sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.wasuta-archive.com/',
+  siteUrl: "https://www.wasuta-archive.com/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: ["/server-sitemap-index.xml", "/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://www.wasuta-archive.com/server-sitemap-index.xml",
+      "https://www.wasuta-archive.com/server-sitemap.xml",
+    ],
+  },
 };
