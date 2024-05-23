124d7e76df7ad9ed1e83913e3fb785623e51e103
k1b3-jp <exfamily0315@gmail.com>
2024-05-23 19:41:34 +0900

    Dynamic Sitemap with Next.js and Supabase

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,11 +3,8 @@ module.exports = {
   siteUrl: "https://www.wasuta-archive.com/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ["/server-sitemap-index.xml", "/server-sitemap.xml"],
+  exclude: ["/sitemap-dynamic.xml"],
   robotsTxtOptions: {
-    additionalSitemaps: [
-      "https://www.wasuta-archive.com/server-sitemap-index.xml",
-      "https://www.wasuta-archive.com/server-sitemap.xml",
-    ],
+    additionalSitemaps: ["https://www.wasuta-archive.com/sitemap-dynamic.xml"],
   },
 };
