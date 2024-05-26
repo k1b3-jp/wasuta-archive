4f44e9e133ec8c06354d493198c1ec7089e9edc6
k1b3-jp <exfamily0315@gmail.com>
2024-05-26 17:18:22 +0900

    change otf to woff

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -6,7 +6,7 @@ export const config = {
 };
 
 const font = fetch(
-  new URL("../../assets/NotoSansJP-Bold.otf", import.meta.url)
+  new URL("../../assets/NotoSansJP-Bold.woff", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
