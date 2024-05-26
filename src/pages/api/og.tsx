d6a5161708d2f44a2c9f1b644fdee3a4311440a3
k1b3-jp <exfamily0315@gmail.com>
2024-05-26 17:59:30 +0900

    fix font loading

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -5,19 +5,38 @@ export const config = {
   runtime: "edge",
 };
 
-const font = fetch(
-  new URL("../../assets/NotoSansJP-Bold.woff", import.meta.url)
-).then((res) => res.arrayBuffer());
+export async function fetchFont(text: string): Promise<ArrayBuffer | null> {
+  const googleFontsUrl = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&text=${encodeURIComponent(
+    text
+  )}`;
+
+  const css = await (
+    await fetch(googleFontsUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
+  ).text();
+
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/
+  );
+
+  if (!resource) return null;
+  const res = await fetch(resource[1]);
+  return res.arrayBuffer();
+}
 
 export default async function handler(req: NextRequest) {
   try {
-    const fontData = await font;
     const { searchParams } = new URL(req.url);
 
     const hasTitle = searchParams.has("title");
-    const title = hasTitle
-      ? searchParams.get("title")?.slice(0, 100)
-      : "My default title";
+    const title = hasTitle ? searchParams.get("title")?.slice(0, 100) : "";
+
+    const font = fetchFont(`${title}わーすたアーカイブ`);
+    const fontData = await font;
 
     const imageURL = searchParams.get("image")?.slice(0, 100);
 
@@ -83,7 +102,7 @@ export default async function handler(req: NextRequest) {
         fonts: [
           {
             name: "NotoSansJP",
-            data: fontData,
+            data: fontData || new ArrayBuffer(0),
             style: "normal",
           },
         ],
