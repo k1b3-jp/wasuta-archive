1e1a17640ff7a65f5decb2cd375bb806ed670225
k1b3-jp <exfamily0315@gmail.com>
2024-05-31 19:32:09 +0900

    format by biome

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -7,51 +7,51 @@ import Logo from "../../../public/logo.svg";
 import BaseButton from "../ui/BaseButton";
 
 const NavBar = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const router = useRouter();
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const router = useRouter();
 
-  const getCurrentUser = async () => {
-    const { data } = await supabase.auth.getSession();
-    if (data.session !== null) {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      setIsLoggedIn(true);
-    }
-  };
+	const getCurrentUser = async () => {
+		const { data } = await supabase.auth.getSession();
+		if (data.session !== null) {
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+			setIsLoggedIn(true);
+		}
+	};
 
-  useEffect(() => {
-    getCurrentUser();
-  }, []);
+	useEffect(() => {
+		getCurrentUser();
+	}, []);
 
-  const Logout = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) throw new Error(error.message);
-    // ログアウトを反映させるためにリロードさせる
-    router.refresh();
-  };
+	const Logout = async () => {
+		const { error } = await supabase.auth.signOut();
+		if (error) throw new Error(error.message);
+		// ログアウトを反映させるためにリロードさせる
+		router.refresh();
+	};
 
-  return (
-    <header className="shadow-lg top-0 z-50">
-      <nav className="flex items-center flex-wrap text-font-color font-bold text-center bg-white">
-        <Link
-          href="/"
-          className="my-2 mr-4 ml-2 inline-flex items-center text-font-color"
-        >
-          <Logo width={50} height={50} />
-        </Link>
-        <div className="bg-white inline-flex flex-row ml-auto w-auto items-center">
-          <div className="inline-flex w-auto px-3 py-4 rounded items-center justify-center">
-            {isLoggedIn ? (
-              <BaseButton onClick={() => Logout()} label="Logout" />
-            ) : (
-              <BaseButton link={"/login"} label={"Login/SignUp"} />
-            )}
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="shadow-lg top-0 z-50">
+			<nav className="flex items-center flex-wrap text-font-color font-bold text-center bg-white">
+				<Link
+					href="/"
+					className="my-2 mr-4 ml-2 inline-flex items-center text-font-color"
+				>
+					<Logo width={50} height={50} />
+				</Link>
+				<div className="bg-white inline-flex flex-row ml-auto w-auto items-center">
+					<div className="inline-flex w-auto px-3 py-4 rounded items-center justify-center">
+						{isLoggedIn ? (
+							<BaseButton onClick={() => Logout()} label="Logout" />
+						) : (
+							<BaseButton link={"/login"} label={"Login/SignUp"} />
+						)}
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
 };
 
 export default NavBar;
--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -2,116 +2,113 @@ import type { NextRequest } from "next/server";
 import { ImageResponse } from "@vercel/og";
 
 export const config = {
-  runtime: "edge",
+	runtime: "edge",
 };
 
 export async function fetchFont(text: string): Promise<ArrayBuffer | null> {
-  const googleFontsUrl = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&text=${encodeURIComponent(
-    text
-  )}`;
+	const googleFontsUrl = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&text=${encodeURIComponent(
+		text,
+	)}`;
 
-  const css = await (
-    await fetch(googleFontsUrl, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
-      },
-    })
-  ).text();
+	const css = await (
+		await fetch(googleFontsUrl, {
+			headers: {
+				"User-Agent":
+					"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+			},
+		})
+	).text();
 
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
+	const resource = css.match(
+		/src: url\((.+)\) format\('(opentype|truetype)'\)/,
+	);
 
-  if (!resource) return null;
-  const res = await fetch(resource[1]);
-  return res.arrayBuffer();
+	if (!resource) return null;
+	const res = await fetch(resource[1]);
+	return res.arrayBuffer();
 }
 
 export default async function handler(req: NextRequest) {
-  try {
-    const { searchParams } = new URL(req.url);
+	try {
+		const { searchParams } = new URL(req.url);
 
-    const hasTitle = searchParams.has("title");
-    const title = hasTitle ? searchParams.get("title") : "";
+		const hasTitle = searchParams.has("title");
+		const title = hasTitle ? searchParams.get("title") : "";
 
-    const font = fetchFont(`${title}わーすたアーカイブ`);
-    const fontData = await font;
+		const font = fetchFont(`${title}わーすたアーカイブ`);
+		const fontData = await font;
 
-    const imageURL = searchParams.get("image");
+		const imageURL = searchParams.get("image");
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundImage:
-              "linear-gradient(160deg, #ffffff 0%, #7bdcb5 100%)",
-            backgroundSize: "100% 100%",
-            height: "100%",
-            width: "100%",
-            display: "flex",
-            textAlign: "left",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            gap: "10px",
-            fontFamily: '"NotoSansJP"',
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexBasis: "45%",
-              flexWrap: "wrap",
-              justifyContent: "flex-start",
-              marginLeft: "40px",
-              color: "#2f2f2f",
-            }}
-          >
-            <div
-              style={{
-                fontSize: 34,
-                marginBottom: 20,
-              }}
-            >
-              🌐 わーすたアーカイブ
-            </div>
-            <div
-              style={{
-                fontSize: 50,
-                lineHeight: 1.1,
-                wordBreak: "keep-all",
-              }}
-            >
-              {title}
-            </div>
-          </div>
-          <img
-            src={imageURL || ""}
-            alt="イベントの様子"
-            style={{
-              flexBasis: "55%",
-              maxHeight: "70%",
-              objectFit: "contain",
-            }}
-          />
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-        fonts: [
-          {
-            name: "NotoSansJP",
-            data: fontData || new ArrayBuffer(0),
-            style: "normal",
-          },
-        ],
-      }
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response("Failed to generate the image", {
-      status: 500,
-    });
-  }
+		return new ImageResponse(
+			<div
+				style={{
+					backgroundImage: "linear-gradient(160deg, #ffffff 0%, #7bdcb5 100%)",
+					backgroundSize: "100% 100%",
+					height: "100%",
+					width: "100%",
+					display: "flex",
+					textAlign: "left",
+					alignItems: "center",
+					justifyContent: "flex-start",
+					gap: "10px",
+					fontFamily: '"NotoSansJP"',
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						flexBasis: "45%",
+						flexWrap: "wrap",
+						justifyContent: "flex-start",
+						marginLeft: "40px",
+						color: "#2f2f2f",
+					}}
+				>
+					<div
+						style={{
+							fontSize: 34,
+							marginBottom: 20,
+						}}
+					>
+						🌐 わーすたアーカイブ
+					</div>
+					<div
+						style={{
+							fontSize: 50,
+							lineHeight: 1.1,
+							wordBreak: "keep-all",
+						}}
+					>
+						{title}
+					</div>
+				</div>
+				<img
+					src={imageURL || ""}
+					alt="イベントの様子"
+					style={{
+						flexBasis: "55%",
+						maxHeight: "70%",
+						objectFit: "contain",
+					}}
+				/>
+			</div>,
+			{
+				width: 1200,
+				height: 630,
+				fonts: [
+					{
+						name: "NotoSansJP",
+						data: fontData || new ArrayBuffer(0),
+						style: "normal",
+					},
+				],
+			},
+		);
+	} catch (e: any) {
+		console.log(`${e.message}`);
+		return new Response("Failed to generate the image", {
+			status: 500,
+		});
+	}
 }
